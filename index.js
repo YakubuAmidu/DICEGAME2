e36b5c0948a20a1d44e1bf5f46c56142d7d510f2
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-02-04 21:49:35 -0800

    Initial commit

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ image1.setAttribute("src", randomImageSource);
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImageSource2 = "img/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "⛳️Player 1 won";
+}
+else if(randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = "⛳️Player 2 won";
+}
+else if(randomNumber1 === randomNumber2){
+  document.querySelector("h1").innerHTML = "It a draw!"
+}
