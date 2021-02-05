040580b80a5fa34557502031e4cc3680b39169b7
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-02-04 21:23:24 -0800

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-alert("Working");
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomimageSource = "img/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomimageSource);
